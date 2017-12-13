c7a741f5a3739463162c6d54e48002c8c4ba6e40
Aaron Wolfcale <aaronwolfcale@Aarons-MacBook-Pro.local>
2017-12-13 15:38:20 -0600

    dark theme & large text

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,8 +5,15 @@
 
 let printUserText = (message) => {
     let userText = document.getElementById("userMessage").value;
-    document.getElementById("outputBox").innerHTML += userText;
+    document.getElementById("outputBox").innerHTML += `${userText}<br><br>`;
 };
+
+function clearUserText () {
+    let userText = document.getElementById("userMessage");
+    userText.value = "";
+}
+
+let userArr = [];
 
 module.exports.pressingEnter = () => {
     let userText = document.getElementById("userMessage");
@@ -15,9 +22,35 @@ module.exports.pressingEnter = () => {
     if (key === 13) {
         // console.log("enter key working");
         printUserText();
+        clearUserText();
     }
     });
 };
+
+
+// ------------------------------------------DARK / LIGHT Themes-------------------------------------------------
+
+
+var darkMode = document.getElementById("fucking-better");
+
+function upsideDown() {
+    darkMode.classList.toggle("light");
+    darkMode.classList.toggle("dark");
+}
+
+document.getElementById("dark").addEventListener("click", upsideDown);
+
+
+// ------------------------------------------TEXT-SIZE-------------------------------------------------
+
+var largeMode = document.getElementById("outputBox");
+
+function largeText() {
+    largeMode.classList.toggle("largeText");
+    largeMode.classList.toggle("smallText");
+}
+
+document.getElementById("large").addEventListener("click", largeText);
 
 
 },{}],3:[function(require,module,exports){
